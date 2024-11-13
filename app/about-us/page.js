31ba022e4b5bee8a6772627573b3d1fc71569ca4
ogbukada emmanuel chisom <emzychisom@gmail.com>
2024-11-13 10:59:38 +0000

    image added on about us

---
+++ app/about-us/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export default function Page () {
+    return (
+        <main className="py-8 md:py-12 1g:py-16 px-2 md:px-8 1g:px-16">
+            <h1 className="text-4x1 mb-4">Iseowo. Freelance market Place for Artisans</h1>
+            <Image width={720} height={380}  src="/code.png" alt="about us picture" />
+        </main>
+    )
+}
